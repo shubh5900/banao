f0872910738b216c44b1b37951a5b384972aa708
shubham singh <43708880+shubh5900@users.noreply.github.com>
2023-12-10 09:11:35 +0530

    Delete Hospital directory

--- Hospital/Base/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render,redirect
-from DoctorDashBoard.models import Doctor
-# Create your views here.
-def Base(request):
-    return render(request,"Base.html")
--- Hospital/PatientDashBoard/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views as Patient
-
-urlpatterns = [
-    path('LoginPage/',Patient.LoginPage ),
-    path('RegistrationPage/',Patient.RegistrationPage),
-    path('DashBoard/',Patient.DashBoard),
-]
